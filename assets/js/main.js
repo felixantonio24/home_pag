55cdbe34f9f5ad45b42cbe8fb3ef940e0c755143
felix <luisfelixtv@outlook.es>
2024-04-15 19:01:17 -0500

    rmss

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,8 @@
+const image = document.querySelector(".menu_mobi")
+const constainerscrool = document.querySelector(".container_mbil_scroll")
+
+
+
 // Datos de ejemplo para las opciones del país
 const countries = ['United States', 'Peru', 'Argentina', 'Ecuador', 'Paraguay'];
 
@@ -13,3 +18,14 @@ for (let i = 0; i < countries.length; i++) {
     // Agrega el <option> al <select>
     selectElement.appendChild(optionElement);
 }
+const imagen = document.querySelector(".menu_mobi");
+const botonCerrar = document.querySelector(".cerrar");
+const containerScroll = document.querySelector(".container_mbil_scroll");
+
+imagen.addEventListener("click", function() {
+    containerScroll.style.display = "block"; // Mostrar el contenedor al hacer clic en la imagen
+});
+
+botonCerrar.addEventListener("click", function() {
+    containerScroll.style.display = "none"; // Ocultar el contenedor al hacer clic en el botón de cerrar
+});
